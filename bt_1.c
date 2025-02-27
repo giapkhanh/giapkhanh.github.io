a9ad5b9833ea33219e650ba343178c54fc3d4fa1
unknown <giapdangkhanh26@gmail.com>
2025-02-27 13:39:06 +0700

    bt_1

---
+++ bt_1.c
@@ -0,0 +1,62 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+    	float x;
+	//Bai 1
+/*	printf("Bai 1: Nhap x:");
+	scanf("%f", &x);
+	printf("%f\n", x*x);
+	printf("%f\n", x*x*x);
+	printf("%f\n", x*x*x*x);
+*/
+
+
+	//Bai 2
+/*	int ngay;
+	int thang;
+	int nam;
+	printf("Bai2:\n");
+	printf("Nhap ngay:");
+	scanf("%d", &ngay);
+	if (!(0 <= ngay <=31)) {
+		printf("Ngay khong hop le!\n");
+	}
+
+	printf("Nhap thang:");
+	scanf("%d", &thang);
+	if (!(0 <= thang <= 12)) {
+		printf("Thang khong hop le!\n");
+	}
+	
+	printf("Nhap nam:");
+	scanf("%d", &nam);
+	if (!(0 <= nam)) {
+		printf("Ngay khong hop le! Nam sinh phai gan day\n");
+	}
+	printf("Dinh dang dd/mm/yyyy:%d/%d/%d\n", ngay, thang, nam);
+*/
+
+
+	//Bai 3
+/*	int a, b;
+	printf("Nhap a:");
+	scanf("%d", &a);
+	printf("Nhap b:");
+	scanf("%d",&b);
+	
+	printf("Day la phep cong: a+b = %d\n", a+b);
+	printf("Day la phep tru: a-b = %d\n", a-b);
+	printf("Day la phep nhan: a*b = %d\n", a*b);
+	printf("Day la phep chia: a/b = %d\n", a/b);
+	printf("Nhan xet: phep chia \'/\' chi lay phan nguyen cua phep chia\n");
+*/	
+	//Bai 4
+/*	float r;
+	printf("Nhap ban kinh cua hinh cau:");
+	scanf("%f", &r);
+	printf("Dien tich hinh cau: %f\n", 4*3.141592654*r*r);
+	printf("The tich hinh cau: %f\n",(float) (4.0 / 3.0)*3.141592654*r*r*r);
+*/	
+    return 0;
+}
